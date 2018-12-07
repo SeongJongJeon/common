0bf756a7e90548f9ff3f74fa7e5474327c836e48
alex <alex@xblocksys.com>
2018-12-07 11:42:18 +0900

    ECC

--- web/api/src/main/java/com/common/web/api/ApiMain.java
+++ web/api/src/main/java/com/common/web/api/ApiMain.java
@@ -1,14 +1,17 @@
 package com.common.web.api;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by alex.
@@ -18,8 +21,43 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching(proxyTargetClass = true)
 @SpringBootApplication(scanBasePackages = {"com.common"}, exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApiMain {
+    @Value("${security.user.name}")
+    private String propertyHello;
+    @Value("${core.env}")
+    private String env;
+
+    public static String parseArguments(String[] args) {
+        String env = "local";
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "dev":
+                    env = "dev";
+                    break;
+                case "prod":
+                    env = "prod";
+                    break;
+            }
+        }
+
+        return env;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiMain.class, args);
+        String env = parseArguments(args);
+
+        new SpringApplicationBuilder(ApiMain.class)
+                .properties(
+                        String.format("spring.config.location:classpath:common-%s.properties,classpath:core-%s.yaml", env, env)
+                )
+                .build()
+                .run(args);
         log.info("Start ApiMain");
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return (a) -> {
+            log.info("CommandLineRunner: " + propertyHello + " : " + env);
+        };
     }
 }
